f0ad9abeb3674162a8c8533790ac0b8bfc058ab5
Jaira McNair <jaimcnair10@gmail.com>
2025-12-12 16:56:25 -0500

    up

--- api/data/[recipe].js
+++ api/data/[recipe].js
@@ -4,21 +4,22 @@ import { neon } from "@neondatabase/serverless";
 
 export default async function handler(req, res) {
   try {
-    const { recipe } = req.query;
+    const { recipe } = req.query; // from URL
 
     const sql = neon(process.env.DATABASE_POSTGRES_URL);
+
     const result = await sql(
       `SELECT * FROM comments WHERE recipe = $1`,
       [recipe]
     );
 
-    // sort results by comment_id
+    // Sort by comment_id
     result.sort((a, b) => a.comment_id - b.comment_id);
 
-    res.status(200).json(result);
-
+    // Return JSON
+    res.status(200).json({ all: result });
   } catch (err) {
-    console.error("Error:", err);
+    console.error(err);
     res.status(500).json({ error: "Internal server error" });
   }
 }
