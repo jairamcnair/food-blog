d28dbbdc0aed2801b98a3803fc344fca7c05ff53
Jaira McNair <jaimcnair10@gmail.com>
2025-12-12 16:11:41 -0500

    up

--- api/data/[recipe].js
+++ api/data/[recipe].js
@@ -1,6 +1,12 @@
 
 
-module.exports = (req, res) => {
-  const { recipe } = req.query;
-  res.json({ recipe });
-};
+const express = require("express");
+const serverlessExpress = require("@vendia/serverless-express");
+
+const app = express();
+
+app.get("/:recipe", (req, res) => {
+  res.json({ recipe: req.params.recipe });
+});
+
+module.exports = serverlessExpress({ app });
